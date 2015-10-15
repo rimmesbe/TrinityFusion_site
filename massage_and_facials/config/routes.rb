11f5a47cbcb762f6bd4fefa9bf13e5380664ea72
rimmesbe <ryanimmesberger@gmail.com>
2015-10-15 16:40:02 -0400

    added some routing

--- massage_and_facials/config/routes.rb
+++ massage_and_facials/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'site#index'
+
+  get 'site' => 'site#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
