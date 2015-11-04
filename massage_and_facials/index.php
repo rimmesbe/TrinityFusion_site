d9ca62c1f0bc7edc723aec27e7ae0c8507953bd4
rimmesbe <ryanimmesberger@gmail.com>
2015-11-04 18:07:30 -0500

    needed to change to index.php, changed contact email

--- massage_and_facials/index.php
+++ massage_and_facials/index.php
@@ -35,7 +35,7 @@
       }
     }
     if($hasError !== true) {
-      $to = "ryanimmesberger@gmail.com";
+      $to = "colleen@oasismindbodyskin.com";
       $msgcontents = "$message";
       $headers = "MIME-Version: 1.0 \r\n";
       $headers .= "Content-type: text/html; charset=iso-8859-1 \r\n";
